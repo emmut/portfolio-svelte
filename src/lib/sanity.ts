61193913d9af3f7873a096abdb528ed8806959e8
Emil Jansson <e.jansson@icloud.com>
2022-02-06 12:01:52 +0100

    ✨ Add urlFor from sanity

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,6 +1,7 @@
 import { env } from 'process';
 
 import createClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 import { api } from '../../studio/sanity.json';
 const { projectId, dataset } = api;
 
@@ -8,7 +9,13 @@ const config = {
 	projectId,
 	dataset,
 	apiVersion: '2022-01-07',
-	useCdn: env.NODE_ENV === 'production'
+	useCdn: !dev
 };
 
 export const client = createClient(config);
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => {
+  return builder.image(source);
+}
