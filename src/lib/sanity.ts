dcd87ddac8137a6954cd4b908e285321c7f7e7e4
Emil Jansson <e.jansson@icloud.com>
2022-02-06 12:02:22 +0100

    ♻ Get env that works on server and client

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,5 +1,4 @@
-import { env } from 'process';
-
+import { dev } from '$app/env';
 import createClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 import { api } from '../../studio/sanity.json';
