c947c087991c62b2e1d33bf69e81d8ec5d0daf86
Emil Jansson <e.jansson@icloud.com>
2022-02-06 12:03:40 +0100

    ✨ Add layout, NavLink and tailwind styles

---
+++ src/lib/components/NavLink.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+	$: isActive = $page.url.pathname === $$props.href;
+</script>
+
+<a href={$$props.href} {...$$props} class:active={isActive}>
+	<slot />
+</a>
+
+<style>
+	.active {
+		@apply underline underline-offset-[3px] decoration-green-500 decoration-2;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import NavLink from '$lib/components/NavLink.svelte';
+	import '$lib/styles/app.css';
+</script>
+
+<header class="bg-zinc-800 text-neutral-200">
+	<span class="text-3xl">
+		<span class="text-green-500">/</span>emmut
+	</span>
+	<nav>
+		<NavLink class="" href="/">Repos</NavLink>
+		<NavLink class="" href="/me">Me</NavLink>
+		<NavLink class="" href="/stuff">Stuff</NavLink>
+	</nav>
+</header>
+
+<main>
+	<slot />
+</main>
