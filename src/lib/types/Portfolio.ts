f2f07d5fd9fe6184186af1c8c04d785d58b869ad
Emil Jansson <e.jansson@icloud.com>
2022-02-27 21:11:41 +0100

    ♻ Use Sanity types for image in portfolio request

--- src/lib/types/Portfolio.ts
+++ src/lib/types/Portfolio.ts
@@ -1,4 +1,5 @@
 import type { PortableTextBlocks } from '@portabletext/svelte/ptTypes';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export type Portfolio = {
   _id: number;
@@ -7,7 +8,10 @@ export type Portfolio = {
     current: string;
     _type: string;
   };
-  image?: string;
+  image?: {
+    asset: SanityImageSource;
+    caption: string;
+  };
   excerpt?: string;
   bio?: PortableTextBlocks;
 };
