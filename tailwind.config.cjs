1adedd2ff6a1d525a32d023687f15c1530d8e753
Emil Jansson <e.jansson@icloud.com>
2022-02-27 14:45:51 +0100

    🔧 Add custom css for typography tailwind plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,22 @@ module.exports = {
       fontFamily: {
         body: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
       },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              fontWeight: 'bold',
+            },
+            '--tw-prose-quote-borders': theme('colors.pink[600]'),
+            '--tw-prose-invert-body': theme('colors.neutral[200]'),
+            '--tw-prose-invert-headings': theme('colors.neutral[200]'),
+            '--tw-prose-invert-bold': theme('colors.neutral[200]'),
+            '--tw-prose-invert-quotes': theme('colors.neutral[200]'),
+            '--tw-prose-invert-quote-borders': theme('colors.green[500]'),
+            '--tw-prose-invert-code': theme('colors.neutral[200]'),
+          },
+        },
+      }),
     },
   },
   plugins: [
