8315ba9a66195facac628b3df7bd5105fff223f3
Emil Jansson <e.jansson@icloud.com>
2022-02-27 21:09:06 +0100

    ✨ Use image component for single portfolio/stuff

--- src/routes/stuff/[slug].svelte
+++ src/routes/stuff/[slug].svelte
@@ -25,12 +25,18 @@
 <script lang="ts">
   import PortableText from '@portabletext/svelte';
   import Link from '$lib/components/Link.svelte';
+  import Image from '$lib/components/SanityImage.svelte';
 
   export let portfolio: Portfolio;
 </script>
 
-<h1 class="mb-9 text-4xl font-bold">{portfolio.title}</h1>
+<Image
+  class="mb-6 w-full xl:-ml-8 xl:w-[calc(100%+4rem)] xl:min-w-[calc(100%+4rem)]"
+  src={portfolio.image}
+  alt={portfolio.image.caption}
+/>
 <div class="prose dark:prose-invert">
+  <h1 class="text-4xl font-bold">{portfolio.title}</h1>
   <PortableText
     blocks={portfolio.bio}
     serializers={{
