aefeeb47255f0e0798c4bf1a4adc2c9c9f176386
Emil Jansson <e.jansson@icloud.com>
2022-04-16 09:12:13 +0200

    ✨ Add info icon for portfolio

---
+++ src/lib/components/PortfolioItem.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import type { Portfolio } from '$lib/types/Portfolio';
+  import { urlFor } from '$lib/sanity';
+  import { deviceDpr, minmax } from '$lib/utils';
+  import Icon from './Icon.svelte';
+
+  export let portfolio: Portfolio;
+  export let index;
+
+  function focusOnPortfolio(event) {
+    event.preventDefault();
+    const portfolio = document.querySelector<HTMLDivElement>(`#portfolio-${index}`);
+    portfolio?.focus();
+  }
+</script>
+
+<a
+  id="portfolio-{index}"
+  sveltekit:prefetch
+  class="group relative aspect-1 {minmax(index, 4, 'col-span-4', 'col-span-8')}"
+  href="/stuff/{portfolio.slug.current}"
+>
+  {#if portfolio.image}
+    <img
+      class="absolute inset-0 -z-10 h-full w-full"
+      src={urlFor(portfolio?.image)
+        .auto('format')
+        .dpr(deviceDpr())
+        .width(minmax(index, 4, 250, 500))
+        .height(minmax(index, 4, 250, 500))
+        .crop('center')
+        .fit('crop')
+        .url()}
+      alt={portfolio.image.caption}
+    />
+  {/if}
+  <div
+    class="h-full bg-gray-800/40 p-4 text-neutral-50 opacity-0 transition-all duration-500 group-focus:opacity-100"
+  >
+    <h2 class="text-xl font-bold">{portfolio.title}</h2>
+    <p class="text-sm line-clamp-5">{portfolio.excerpt}</p>
+  </div>
+
+  <div class="info-icon" tabindex="0" on:click={focusOnPortfolio}>
+    <Icon name="info" />
+  </div>
+</a>
+
+<style lang="postcss">
+  .info-icon {
+    /* Position */
+    @apply absolute 
+      top-2 
+      right-2;
+    /* Layout */
+    @apply flex 
+      h-6 
+      w-6 
+      items-center 
+      justify-center
+      p-1;
+    /* Appearance */
+    @apply cursor-pointer 
+      rounded-full 
+      bg-neutral-200 
+      font-bold 
+      text-gray-900;
+    /* Hover */
+    @apply hover:bg-neutral-100;
+    /* Focus */
+    @apply focus:bg-neutral-100 
+      focus:ring-2;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,8 +22,7 @@
 
 <script lang="ts">
   import type { Portfolio } from '$lib/types/Portfolio';
-  import { urlFor } from '$lib/sanity';
-  import { deviceDpr, minmax } from '$lib/utils';
+  import PortfolioItem from '$lib/components/PortfolioItem.svelte';
 
   export let portfolios: Portfolio[];
 </script>
@@ -33,32 +32,7 @@
 </svelte:head>
 
 <div class="grid grid-cols-8 lg:grid-cols-12">
-  {#each portfolios as portfolio, i}
-    <a
-      sveltekit:prefetch
-      class="group relative aspect-1 {minmax(i, 4, 'col-span-4', 'col-span-8')}"
-      href="/stuff/{portfolio.slug.current}"
-    >
-      {#if portfolio.image}
-        <img
-          class="absolute inset-0 -z-10 h-full w-full"
-          src={urlFor(portfolio?.image)
-            .auto('format')
-            .dpr(deviceDpr())
-            .width(minmax(i, 4, 250, 500))
-            .height(minmax(i, 4, 250, 500))
-            .crop('center')
-            .fit('crop')
-            .url()}
-          alt={portfolio.image.caption}
-        />
-      {/if}
-      <div
-        class="h-full bg-gray-800/40 p-4 text-neutral-50 opacity-0 transition-all duration-500 group-hover:opacity-100 group-focus:opacity-100"
-      >
-        <h2 class="text-xl font-bold">{portfolio.title}</h2>
-        <p class="text-sm line-clamp-5">{portfolio.excerpt}</p>
-      </div>
-    </a>
+  {#each portfolios as portfolio, index}
+    <PortfolioItem {portfolio} {index} />
   {/each}
 </div>
