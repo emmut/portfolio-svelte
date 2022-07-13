3ec2fa3e55ccf0eff1c168aef4ff3e7c8224aace
Emil Jansson <e.jansson@icloud.com>
2022-07-13 13:44:57 +0200

    💄 Add width and height to image

--- src/routes/stuff/[slug].svelte
+++ src/routes/stuff/[slug].svelte
@@ -38,6 +38,8 @@
   class="mb-6 w-full xl:-ml-8 xl:w-[calc(100%+4rem)] xl:min-w-[calc(100%+4rem)]"
   src={portfolio.image}
   alt={portfolio.image.caption}
+  width="1000"
+  height="750"
 />
 <div class="prose mx-auto dark:prose-invert">
   <h1 class="text-4xl font-bold">{portfolio.title}</h1>
