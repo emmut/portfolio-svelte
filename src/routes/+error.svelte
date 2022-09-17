a0afe4c304a3b14e37d4bfb62d46b4b8cccfc9a9
Emil Jansson <e.jansson@icloud.com>
2022-09-17 18:17:59 +0200

    💡 Update comment

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,4 @@
-<!-- TODO: Make nicer -->
+<!-- TODO: Make look nicer -->
 <div class="prose text-center dark:prose-invert">
   <h1 class="text-2xl font-bold">Page was not found</h1>
   <a href="/">Go back to home page</a>
