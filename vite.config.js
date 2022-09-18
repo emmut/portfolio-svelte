6ec07d8a95f75cfdb2363ef5bc3a07689a6a56e5
Emil Jansson <e.jansson@icloud.com>
2022-09-18 17:44:25 +0200

    🚚 Rename vite config file .js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import type { UserConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-const config: UserConfig = {
+/** @type {import('vite').UserConfig} */
+const config = {
   server: {
-    host: true,
-    // port: 8080,
     fs: {
       allow: ['./studio'],
     },
