1998987772c00fd8232794a7f7187d673b422eab
Emil Jansson <e.jansson@icloud.com>
2022-09-18 17:42:23 +0200

    ✨ Add heights for Sanity images

--- src/lib/components/SanityImage.svelte
+++ src/lib/components/SanityImage.svelte
@@ -3,13 +3,13 @@
   import { deviceDpr } from '$lib/utils';
   import type { CropMode, FitMode, SanityImageSource } from '@sanity/image-url/lib/types/types';
 
-  // TODO: Add height option
   export let sizes = [1500, 1000, 500];
   export let breakpoints = ['min-width: 1500px', 'min-width: 500px', 'max-width: 499px'];
   export let src: SanityImageSource;
   export let quality = 85;
   export let crop: CropMode = 'center';
   export let fit: FitMode = 'crop';
+  const aspectRatio = $$props.height / $$props.width;
 </script>
 
 {#if src}
@@ -19,6 +19,7 @@
         media="({breakpoints[i]})"
         srcset={src &&
           urlFor(src)
+            .height(Math.floor(size * aspectRatio))
             .width(size)
             .quality(quality)
             .auto('format')
@@ -32,6 +33,7 @@
       class={$$props.class}
       src={src &&
         urlFor(src)
+          .height(Math.floor(sizes[sizes.length - 1] * aspectRatio))
           .width(sizes[sizes.length - 1])
           .quality(quality)
           .url()}
