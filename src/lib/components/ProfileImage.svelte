619302f75c269b97ad63b50de9855d15c7b5e1ce
Emil Jansson <e.jansson@icloud.com>
2022-12-17 13:54:20 +0100

    💄 Style profile picture ring

--- src/lib/components/ProfileImage.svelte
+++ src/lib/components/ProfileImage.svelte
@@ -3,4 +3,4 @@
   export let alt: string;
 </script>
 
-<img class="h-28 w-28 rounded-full ring-4 ring-neutral-100/70" {src} {alt} />
+<img class="h-28 w-28 rounded-full ring-4 ring-zinc-300/70 dark:ring-neutral-100/70" {src} {alt} />
