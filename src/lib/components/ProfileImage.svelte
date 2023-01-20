d6f610cd7e32e398347dfe1b1b057527372a0843
Emil Jansson <e.jansson@icloud.com>
2023-01-20 19:32:56 +0100

    🔥 Remove ProfileImage component

--- src/lib/components/ProfileImage.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  export let src: string;
-  export let alt: string;
-</script>
-
-<img class="h-28 w-28 rounded-full ring-4 ring-zinc-300/70 dark:ring-neutral-100/70" {src} {alt} />
