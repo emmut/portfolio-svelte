012aa345ec4477699beeb27cd35fd190bd1eb35e
Emil Jansson <e.jansson@icloud.com>
2023-03-21 18:43:59 +0100

    🔥 Remove comment

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: 'ql7rbeue', // replace value with your own
-    dataset: 'production', // replace value with your own
+    projectId: 'ql7rbeue',
+    dataset: 'production',
   },
 });
