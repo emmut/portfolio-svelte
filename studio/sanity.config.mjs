aa9fce610ffa6e1bc2f85c5ed7695658a6fef174
Emil Jansson <ejansson.swe@gmail.com>
2023-06-28 13:46:45 +0200

    🔧 Use import meta for loading env vars in studio¨

--- studio/sanity.config.mjs
+++ studio/sanity.config.mjs
@@ -3,13 +3,12 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import schemas from './schemas/schema';
 import deskStructure from './deskStructure';
-import { env } from 'process';
 
 export default defineConfig({
   name: 'emmut',
   title: 'emmut',
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: process.env.SANITY_STUDIO_DATASET || 'production',
+  projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: import.meta.env.SANITY_STUDIO_DATASET || 'production',
   plugins: [
     deskTool({
       structure: deskStructure,
