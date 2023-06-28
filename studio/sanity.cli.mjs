56e95190cdcefba1414d5af3814b945793703158
Emil Jansson <ejansson.swe@gmail.com>
2023-06-28 15:46:42 +0200

    🚚 Rename sanity cli config to js form mjs

--- studio/sanity.cli.mjs
+++
@@ -1,8 +0,0 @@
-import { defineCliConfig } from 'sanity/cli';
-
-export default defineCliConfig({
-  api: {
-    projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: import.meta.env.SANITY_STUDIO_DATASET,
-  },
-});
