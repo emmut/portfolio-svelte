4d9fe2483b07ccff41b18227555ad68448d4da25
Emil Jansson <ejansson.swe@gmail.com>
2023-06-28 16:11:16 +0200

    🔧 Remove tailwind plugin for line clamp

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -30,9 +30,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')],
 };
