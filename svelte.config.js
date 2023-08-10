870de20b0b9195ec72b790b10b0d74670b906e43
Emil Jansson <ejansson.swe@gmail.com>
2023-08-10 20:27:53 +0200

    💡 Remove todo comment

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
-/** @todo Make this app SSG */
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
